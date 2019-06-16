ee347a93fd3bcf69bcc446c83a0b243e3fa3b078
AregevDev <aregevdev@gmail.com>
2019-06-16 20:24:08 +0300

    Fixed issue with VertexArray Finally!!!

--- src/vertexbufferlayout.cpp
+++ src/vertexbufferlayout.cpp
@@ -3,18 +3,18 @@
 template<>
 void VertexBufferLayout::push<float>(unsigned int count) {
     elements.push_back({ GL_FLOAT, count, GL_FALSE });
-    stride += VertexBufferElement::getSizeOfType(GL_FLOAT);
+    stride += VertexBufferElement::getSizeOfType(GL_FLOAT) * count;
     std::cout << stride << std::endl;
 }
 
 template<>
 void VertexBufferLayout::push<unsigned int>(unsigned int count) {
     elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
-    stride += VertexBufferElement::getSizeOfType(GL_UNSIGNED_INT);
+    stride += VertexBufferElement::getSizeOfType(GL_UNSIGNED_INT) * count;
 }
 
 template<>
 void VertexBufferLayout::push<unsigned char>(unsigned int count) {
     elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
-    stride += VertexBufferElement::getSizeOfType(GL_UNSIGNED_BYTE);
+    stride += VertexBufferElement::getSizeOfType(GL_UNSIGNED_BYTE) * count;
 }
