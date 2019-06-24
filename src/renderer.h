e07ac2449aea403e6b4b0c01ac7674b59502323b
AregevDev <aregevdev@gmail.com>
2019-06-24 11:07:57 +0300

    Remove static from draw method

--- src/renderer.h
+++ src/renderer.h
@@ -9,7 +9,7 @@ class Renderer {
 private:
 
 public:
-    static void draw(const VertexArray &vao, const IndexBuffer &ibo, const Shader &shader);
+    void draw(const VertexArray &vao, const IndexBuffer &ibo, const Shader &shader);
     void clearBackground(float r, float g, float b, float a) const;
 };
 
